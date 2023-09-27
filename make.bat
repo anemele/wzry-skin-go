b4d0dc8415020781cd3e9b089e214b9ce4076ef6
Jason Swift <anemele@outlook.com>
2023-09-27 17:54:14 +0800

    update make.bat

--- make.bat
+++ make.bat
@@ -4,9 +4,10 @@ setlocal
 
 set dist=dist
 set target=wzry-skin
+set build=go.exe build -ldflags="-s -w"
 
 rem build for Windows
-go build -o %dist%/windows/%target%.exe
+%build% -o %dist%/windows/%target%.exe
 
 rem cross platform compile
 set CGO_ENABLED=0
@@ -14,8 +15,8 @@ set GOARCH=amd64
 
 rem build for Linux
 set GOOS=linux
-go build -o %dist%/linux/%target%
+%build% -o %dist%/linux/%target%
 
 rem build for macOS
 set GOOS=darwin
-go build -o %dist%/darwin/%target%
+%build% -o %dist%/darwin/%target%
