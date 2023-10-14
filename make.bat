23a3d412408a9d935045d866bc0bad0f30e683e8
Jason Swift <anemele@outlook.com>
2023-10-14 17:19:51 +0800

    update make.bat

--- make.bat
+++ make.bat
@@ -6,17 +6,43 @@ set dist=dist
 set target=wzry-skin
 set build=go.exe build -ldflags="-s -w"
 
-rem build for Windows
-%build% -o %dist%/windows/%target%.exe
+if [%~1] == [clean] goto lbl-clean
+if [%~1] == [windows] goto lbl-windows
+if [%~1] == [linux] goto lbl-linux
+if [%~1] == [darwin] goto lbl-darwin
+goto lbl-build
 
+:lbl-clean
+rd /s /q %dist%
+exit /b
+
+:lbl-build
+call :lbl-windows
+call :lbl-linux
+call :lbl-darwin
+exit /b
+
+:lbl-cross-platform
 rem cross platform compile
-set CGO_ENABLED=0
-set GOARCH=amd64
+if not defined CGO_ENABLED set CGO_ENABLED=0
+if not defined GOARCH set GOARCH=amd64
+exit /b
 
+:lbl-windows
+rem build for Windows
+%build% -o %dist%/%target%-windows.exe
+exit /b
+
+:lbl-linux
 rem build for Linux
+call :lbl-cross-platform
 set GOOS=linux
-%build% -o %dist%/linux/%target%
+%build% -o %dist%/%target%-linux
+exit /b
 
+:lbl-darwin
 rem build for macOS
+call :lbl-cross-platform
 set GOOS=darwin
-%build% -o %dist%/darwin/%target%
+%build% -o %dist%/%target%-darwin
+exit /b
