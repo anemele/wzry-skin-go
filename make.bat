78ea25a42bdf510fc85f38045e36d9410767f42a
Jason Swift <anemele@outlook.com>
2024-12-15 15:21:06 +0800

    update `make.bat`

--- make.bat
+++ make.bat
@@ -4,7 +4,7 @@ setlocal
 
 set dist=dist
 set target=wzry-skin
-set build=go.exe build -ldflags="-s -w"
+set build=go.exe build -ldflags="-s -w" -trimpath
 
 if "%~1" == "clean" goto lbl-clean
 if "%~1" == "windows" goto lbl-windows
@@ -30,6 +30,8 @@ exit /b
 
 :lbl-windows
 rem build for Windows
+call :lbl-cross-platform
+set GOOS=windows
 %build% -o %dist%/%target%-windows.exe
 exit /b
 
