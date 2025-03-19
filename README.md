833bba65496e08f92e16959c6773629525421e1b
Jason Swift <anemele@outlook.com>
2025-03-19 10:00:26 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 [王者荣耀英雄列表](https://pvp.qq.com/web201605/herolist.shtml)
+
+保存路径：
+默认 `wzry-skin` ，可以创建一个 `savepath.txt` 文件，写入保存路径覆盖默认路径。
--- main.go
+++ main.go
@@ -6,43 +6,91 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"path"
+	"strings"
 	"sync"
 )
 
-var client http.Client
-var wg sync.WaitGroup
+const defaultSavePath = "wzry-skin"
+const savePathFile = "savepath.txt"
 
-var rootDir = "wzry-skin"
+func getSavePath() string {
+	file, err := os.Open(savePathFile)
+	if err != nil {
+		return defaultSavePath
+	}
+	buf, err := io.ReadAll(file)
+	if err != nil {
+		return defaultSavePath
+	}
+	return string(buf)
+}
+
+type Data struct {
+	Bytes []byte
+	Path  string
+}
+
+var data = make(chan Data, 5)
+
+func processHero(savePath string, hero Hero, client *http.Client, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	var heroDir = path.Join(
+		savePath, fmt.Sprintf("%s_%s", hero.CName, hero.Title),
+	)
+	os.Mkdir(heroDir, os.ModePerm)
+
+	for i, skin := range strings.Split(hero.Skins, "|") {
+		i += 1
+		url := getSkinUrl(hero.EName, i)
+		fp := path.Join(
+			heroDir,
+			fmt.Sprintf("%02d_%s.jpg", i, skin),
+		)
+
+		wg.Add(2)
+		go func() {
+			defer wg.Done()
+			res, _ := client.Get(url)
+			buf, _ := io.ReadAll(res.Body)
+			data <- Data{buf, fp}
+		}()
+		go func() {
+			defer wg.Done()
+			d := <-data
+			file, _ := os.Create(d.Path)
+			defer file.Close()
+			file.Write(d.Bytes)
+			fmt.Println("done: ", d.Path)
+		}()
+
+	}
+}
 
 func main() {
-	if !exists(rootDir) {
-		os.Mkdir(rootDir, os.ModePerm)
-	}
+	savePath := getSavePath()
+	os.MkdirAll(savePath, os.ModePerm)
 
-	var res, err = client.Get(heroUrl)
+	var client http.Client
+	var resp, err = client.Get(heroUrl)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	buf, err := io.ReadAll(res.Body)
-	if err != nil {
-		fmt.Println("failed to read remote data:", err)
-		return
-	}
-	// fmt.Println(buf)
-
 	var herolist []Hero
-	err = json.Unmarshal(buf, &herolist)
+	err = json.NewDecoder(resp.Body).Decode(&herolist)
 	if err != nil {
 		fmt.Println("failed to parse hero data:", err)
 		return
 	}
 	// fmt.Println(herolist)
 
+	var wg sync.WaitGroup
+	wg.Add(len(herolist))
 	for _, hero := range herolist {
-		wg.Add(1)
-		go processHero(hero)
+		go processHero(savePath, hero, &client, &wg)
 	}
 
 	wg.Wait()
